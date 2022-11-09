bb2e41722da814d015907887b0d2315be37015cd
anushree.shanke <anushree.shanke@SYSLP1385>
2022-11-09 13:21:12 +0530

    Add project files.

---
+++ ConsoleApp1/Course.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FirstApp
+{
+    public class Course
+    {
+        public int CourseID { get; set; }
+        public string Title { get; set; }
+        public int Credits { get; set; }
+    }
+}
